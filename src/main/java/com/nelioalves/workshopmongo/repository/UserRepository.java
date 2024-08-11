642325621d28eb5124f87ffe33a4646f321f448e
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2024-08-11 17:04:27 -0400

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/nelioalves/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.nelioalves.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.nelioalves.workshopmongo.domain.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
